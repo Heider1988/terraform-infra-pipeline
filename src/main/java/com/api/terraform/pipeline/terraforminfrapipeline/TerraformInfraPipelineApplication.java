8e699b75b11ae06028a322c9e4bbae0b54bd26a3
Heider Oliveira <heider.o@icloud.com>
2025-05-22 17:02:35 -0300

    Add initial Spring Boot project setup with Maven wrapper
    This commit includes the setup for a new Spring Boot project named "Terraform-Infra-Pipeline." It includes Maven wrapper files, basic application properties, project structure, and dependencies for a web module and testing. Additionally, it includes configuration files like `.gitignore` and `.gitattributes` for project management.

---
+++ src/main/java/com/api/terraform/pipeline/terraforminfrapipeline/TerraformInfraPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.api.terraform.pipeline.terraforminfrapipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraformInfraPipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerraformInfraPipelineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/terraform/pipeline/terraforminfrapipeline/TerraformInfraPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.terraform.pipeline.terraforminfrapipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerraformInfraPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
