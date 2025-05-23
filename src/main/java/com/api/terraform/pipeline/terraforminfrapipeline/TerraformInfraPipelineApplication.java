744d8cdfb593743ec2beb50ed7783748d5857315
Heider Oliveira <heider.o@icloud.com>
2025-05-22 21:23:57 -0300

    feat: add /time API endpoint to return formatted current time
    - Introduced `TesteController` with a new `/api/time` GET endpoint
    - Created `TimeResponse` record to encapsulate the time response
    - Updated application to scan new packages for controller and DTOs

--- src/main/java/com/api/terraform/pipeline/terraforminfrapipeline/TerraformInfraPipelineApplication.java
+++ src/main/java/com/api/terraform/pipeline/terraforminfrapipeline/TerraformInfraPipelineApplication.java
@@ -2,8 +2,10 @@ package com.api.terraform.pipeline.terraforminfrapipeline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.api.terraform.pipeline.terraforminfrapipeline", "controller", "dto"})
 public class TerraformInfraPipelineApplication {
 
     public static void main(String[] args) {
